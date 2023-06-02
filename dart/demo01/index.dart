638c50efde5e1c2d2ba3ee526fb1ce3fd2a4938d
thenami <2425975054@qq.com>
2023-06-02 18:36:35 +0800

    更新 index.dart
    dartdemo

--- dart/demo01/index.dart
+++ dart/demo01/index.dart
@@ -6,7 +6,17 @@
 //   print("void代表没有返回值");
 // }
 
-void main (){
-  var str = "你好dart";
-  print(str);
-}
+// void main (){
+//   var str = "你好dart";
+//   print(str);
+// }
+
+//2.字符串拼接
+
+// void main() {
+//   String str1 = "你好";
+//   String str2 = "dart";
+//   print("$str1 $str2"); //字符串拼接的第一种方式
+//   print(str1 +" " + str2);
+// }
+
